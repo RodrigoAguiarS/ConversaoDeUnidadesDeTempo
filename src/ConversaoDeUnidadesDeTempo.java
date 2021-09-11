b806809350c64bb7da00ba5ae77da5b4c4d15c19
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-11 03:08:55 -0300

    segundo commit

--- src/ConversaoDeUnidadesDeTempo.java
+++ src/ConversaoDeUnidadesDeTempo.java
@@ -9,6 +9,10 @@ public class ConversaoDeUnidadesDeTempo {
     public static float converteHorasParaSegundos(float horas){
         return horas * constanteDeTempo *constanteDeTempo;
     }
+    //MINUTOS -> HORAS
+    public static float converteMinutosParaHoras(float minutos){
+        return minutos / constanteDeTempo;
+    }
 
 
 }
