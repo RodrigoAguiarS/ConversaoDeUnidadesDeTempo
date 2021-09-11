7ad776e46b84ef8fffea4b1d9ba83882b2a50586
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-09-11 09:56:03 -0300

    inserindo o main

--- src/ConversaoDeUnidadesDeTempo.java
+++ src/ConversaoDeUnidadesDeTempo.java
@@ -13,6 +13,18 @@ public class ConversaoDeUnidadesDeTempo {
     public static float converteMinutosParaHoras(float minutos){
         return minutos / constanteDeTempo;
     }
+    //MINUTOS -> SEGUNDOS
+    public static float converteMinuntosParaSegundos(float minutos){
+        return minutos * constanteDeTempo;
+    }
+    //SEGUNDOS -> MINUTOS
+    public static float converteSegundosParaMinutos(float segundos){
+        return segundos / constanteDeTempo;
+    }
+    //SEGUNDOS -> HORAs
+    public static float converteSegundosParaHoras (float segundos){
+        return segundos / constanteDeTempo / constanteDeTempo;
+    }
 
 
 }
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ConversaoDeUnidadesDeTempo.converteSegundosParaHoras(3600));
+        System.out.println();
+
+    }
+}
